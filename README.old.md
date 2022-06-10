b9b03725fbaac4d88dd0760fe0c6996f5a545c08
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-06-10 16:37:21 +0100

    added react app, added landing page, added contact contact information

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# coming-soon
+
+This is a page which will temporarily act as landing page for "page under construction" for a local church website.
---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import  Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        {/* <img src={} className="App-logo" alt="logo" /> */}
+        <h3>Site Under construction</h3>
+        <h1>
+          Grace Empowerment Assembly
+        </h1>
+         <h3>This is a church where the light of God shines every second</h3>
+         <h3>To get in contac with us:</h3>
+         <p>email us at : info@graceempowermentassembly.com</p>
+         <p>Mobile 1 : 07984413361</p>
+         <p>Mobile 2 : 07494052360</p>
+
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
